aa1c50793a06615245de7de0ae8810aa8dba04b6
kumarviresh25 <104244569+kumarviresh25@users.noreply.github.com>
2022-05-24 17:09:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-sudo apt update
-java -version
-sudo apt install -y default-jre
-
-java -version
-sudo apt install-y default-jdk
-
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export PATH=$PATH:$JAVA_HOME/bin
-echo $PATH
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install -y jenkins
-sudo apt  install docker.io
-
-
+cd /edx/app/edxapp/edx-platform
+sudo -H -u edxapp bash
+source ../edxapp_env
+paver update_assets --settings=production
+exit
+sudo /edx/bin/supervisorctl restart all
